bec35b8ccd0cfa0f12398aea0fe52b8f049e266c
Mateusz <mat.pulka@gmail.com>
2017-02-11 13:28:20 +0100

    Added solution for problem 8

--- src/main/java/com/eurela/project/App.java
+++ src/main/java/com/eurela/project/App.java
@@ -1,12 +1,9 @@
 package com.eurela.project;
 
-import com.eurela.project.problem_7.Solution;
+import com.eurela.project.problem_8.Solution;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println(Solution.compute(10_001));
+        System.out.println(Solution.compute(13));
     }
 }
