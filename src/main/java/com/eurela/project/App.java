e416c336c75f4ae25c591a88d031c15a25b67286
Mateusz <mat.pulka@gmail.com>
2017-02-13 20:21:35 +0100

    Added solution for problem 13

--- src/main/java/com/eurela/project/App.java
+++ src/main/java/com/eurela/project/App.java
@@ -1,9 +1,9 @@
 package com.eurela.project;
 
-import com.eurela.project.problem_12.Solution;
+import com.eurela.project.problem_13.Solution;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(Solution.compute(500));
+        System.out.println(Solution.compute());
     }
 }
